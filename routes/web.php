aad3cad8a52838bb7d849b21623e60f062c84ca9
Miranda del Sol <eidel@epiaonce.cu>
2019-08-08 13:31:22 -0400

    Commit Inicial

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'InicioController@index')->name('inicio');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+    /*RUTAS DE PERMISO*/
+    Route::get('permiso', 'PermisoController@index')->name('permiso');
+    Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
+    /*RUTAS DE MENU*/
+    Route::get('menu', 'MenuController@index')->name('menu');
+    Route::get('menu/crear', 'MenuController@crear')->name('crear_menu');
+    Route::post('menu', 'MenuController@guardar')->name('guardar_menu');
+});
